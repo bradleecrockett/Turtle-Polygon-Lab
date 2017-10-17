3bbc97c556db2fcd6a4c3c6e6b39eb3b7e8b3b3f
bradleecrockett <bcrockett@rjuhsd.us>
2017-10-16 23:14:14 -0700

    Add files via upload

---
+++ turtle_polygon.py
@@ -0,0 +1,24 @@
+import turtle
+
+# Create a new turtle pen Object
+t = turtle.Pen()
+
+def polygon(len: float, sides: int):
+    """Use a for loop to draw a polygon with a given side length and # of sides"""
+    for x in range('''missing code'''):
+        t.forward('''missing code''')
+        t.left('''missing code''')
+
+def main():
+    # call the polygon function to make a square with a side length of 50
+    '''missing code'''
+    # call the polygon function to make a pentagon with a side length of 100
+    '''missing code'''
+    # call the polygon function to make a hexagon with a side length of 150
+    '''missing code'''
+
+# call the main() function
+main()
+
+# Keep the window open at the end of the program
+turtle.mainloop()
